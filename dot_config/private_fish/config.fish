6e5e2aa08d03a8d6591ab0aebc96328e906010c0
Johannes <zaunrithj@gmail.com>
2026-02-26 01:58:13 +0100

    updates

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,5 +7,7 @@ abbr -a cm chezmoi
 abbr -a lg lazygit
 abbr -a ip nvim ~/.local/share/chezmoi/.chezmoidata/packages.yaml
 
+fish_default_key_bindings
+
 # Created by `pipx` on 2026-02-04 23:35:39
 set PATH $PATH /home/xinoi/.local/bin
